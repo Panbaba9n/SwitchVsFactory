26d3c8ba1976f2b18f9a9390757ba81ad6e85364
Panbaba9n <Goldgraalar@gmail.com>
2020-04-13 17:31:36 +0300

    Update switch to object literal

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { MyColorsService } from './my-colors.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  availableColors: string[] = [
+    // 'plum',
+    'thistle',
+    'floral',
+    'lollipop',
+    'teal',
+    'carnation',
+    'bubblegum',
+    'ruby',
+    'lawn',
+    'honeydew',
+    'mindaro',
+    'pistachio',
+    'yellow',
+    'cream',
+    'banana',
+    'trombone',
+    // 'red',
+    // 'orange',
+    // 'plasmass'
+  ];
+
+  // displayColors: string[] = [];
+
+  constructor(private colors: MyColorsService) {
+    for (let i = 1; i <= 500; i++) {
+      var item = this.availableColors[Math.floor(Math.random() * this.availableColors.length)];
+      // this.displayColors.push(item);
+      this.colors.addColor(item);
+    }
+  }
+}
